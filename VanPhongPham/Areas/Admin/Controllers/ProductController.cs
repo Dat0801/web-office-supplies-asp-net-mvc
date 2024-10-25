f6199605a057c9203d54cf5829a8b571e349a8db
Nguyễn Từ Thành Đạt <nttd0801@gmail.com>
2024-10-25 18:01:10 +0700

    create product form

---
+++ VanPhongPham/Areas/Admin/Controllers/ProductController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using VanPhongPham.Models;
+namespace VanPhongPham.Areas.Admin.Controllers
+{
+    public class ProductController : Controller
+    {
+        // GET: Admin/Product
+        private readonly ProductRepository productRepository;
+
+        public ProductController()
+        {
+            productRepository = new ProductRepository();
+        }
+
+        public ActionResult Index()
+        {
+            List<product> listProduct = productRepository.GetAllProducts();
+            return View(listProduct);
+        }
+    }
+}
---
+++ VanPhongPham/Models/ProductRepository.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace VanPhongPham.Models
+{
+    public class ProductRepository
+    {
+        private readonly DB_VanPhongPhamDataContext _context;
+
+        public ProductRepository()
+        {
+            _context = new DB_VanPhongPhamDataContext();
+        }
+
+        public List<product> GetAllProducts()
+        {
+            return _context.products.ToList();
+        }
+    }
+}
