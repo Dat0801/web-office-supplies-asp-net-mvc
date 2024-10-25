93f622411e858fbc91b4e7caeba63dce26248fbd
Nguyễn Từ Thành Đạt <nttd0801@gmail.com>
2024-10-25 15:00:59 +0700

    create manage suppliers

---
+++ VanPhongPham/Areas/Admin/Controllers/SupplierController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using VanPhongPham.Models;
+namespace VanPhongPham.Areas.Admin.Controllers
+{
+    public class SupplierController : Controller
+    {
+        private readonly SupplierRepository supplierRepository;
+        public SupplierController()
+        {
+            supplierRepository = new SupplierRepository();
+        }
+        // GET: Admin/Supplier
+        public ActionResult Index()
+        {
+            List<supplier> listSupplier = supplierRepository.GetAllSuppliers();
+            return View(listSupplier);
+        }
+
+        [HttpGet]
+        public ActionResult AddSupplier()
+        {
+            return View();
+        }
+    }
+}
---
+++ VanPhongPham/Models/SupplierRepository.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using VanPhongPham.Models;
+namespace VanPhongPham.Models
+{
+    public class SupplierRepository
+    {
+        private readonly DB_VanPhongPhamDataContext _context;
+
+        public SupplierRepository ()
+        {
+            _context = new DB_VanPhongPhamDataContext();
+        }
+
+        public List<supplier> GetAllSuppliers()
+        {
+            return _context.suppliers.ToList();
+        }
+    }
+}
