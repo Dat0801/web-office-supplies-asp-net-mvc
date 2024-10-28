5c93d417d53d3c0ed9083bff823c2a908ba7b112
Nguyễn Từ Thành Đạt <nttd0801@gmail.com>
2024-10-28 10:29:30 +0700

    create form supplier and product

---
+++ VanPhongPham/Models/UserRepository.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace VanPhongPham.Models
+{
+    public class UserRepository
+    {
+        private readonly DB_VanPhongPhamDataContext _context;
+        public UserRepository()
+        {
+            _context = new DB_VanPhongPhamDataContext();
+        }
+        public List<user> GetAllUsers()
+        {
+            return _context.users.ToList();
+        }
+        public user GetUserById(string id)
+        {
+            return _context.users.FirstOrDefault(x => x.user_id == id);
+        }
+        public void AddUser(user user)
+        {
+            _context.users.InsertOnSubmit(user);
+            _context.SubmitChanges();
+        }
+        public void UpdateUser(user user)
+        {
+            
+            _context.SubmitChanges();
+        }
+        public void DeleteUser(string id)
+        {
+            user userToDelete = GetUserById(id);
+            _context.users.DeleteOnSubmit(userToDelete);
+            _context.SubmitChanges();
+        }
+    }
+}
